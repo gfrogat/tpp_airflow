157d04f0857e28b6d44f7b264d1ef9e550e4e82c
Peter Ruch <gfrogat@gmail.com>
2019-08-16 17:55:09 +0200

    Refactor ZINC15 download

--- datasets/ZINC15/download.py
+++ datasets/ZINC15/download.py
@@ -1,3 +1,4 @@
+import argparse
 import logging
 import os
 from pathlib import Path
@@ -5,6 +6,9 @@ from pathlib import Path
 import pandas as pd
 import requests
 from tqdm import tqdm
+
+_zinc15_dir = Path("/publicdata/tpp/datasets/ZINC15")
+_sdf_path = _zinc15_dir / "sdf"
 
 _zinc15_url = (
     "https://zinc15.docking.org/activities.sdf"
@@ -14,24 +18,23 @@ _zinc15_n_items = 638174
 _zinc15_items_per_page = 100
 
 
-def get_page_as_sdf(page, sdf_path, url=_zinc15_url):
-    payload = {"page": page}
-    r = requests.get(url, params=payload)
+def get_page_as_sdf(page: int, sdf_path: Path, url: str = _zinc15_url):
+    r = requests.get(url, params={"page": page})
     if r.status_code == requests.codes.ok:
-        with open(sdf_path / "page{}.sdf".format(page), "wb") as sdf:
+        page_path = sdf_path / f"page{page}"
+        with open(page_path.as_posix(), "wb") as sdf:
             sdf.write(r.content)
 
 
-def download_zinc15(sdf_path="/publicdata/tpp/ZINC15/ZINC15.sdf"):
+def download_zinc15(sdf_path: Path):
     logging.basicConfig(level=logging.DEBUG)
 
     # compute number of pages
     n_pages = _zinc15_n_items // _zinc15_items_per_page + 1
 
     # create new folder for SDF files
-    sdf_path = Path(sdf_path)
-    if not os.path.exists(sdf_path):
-        os.mkdir(sdf_path)
+    if not sdf_path.exists():
+        sdf_path.mkdir()
 
     pages = range(1, 1 + n_pages)
     failed_pages = []
@@ -48,13 +51,26 @@ def download_zinc15(sdf_path="/publicdata/tpp/ZINC15/ZINC15.sdf"):
             logging.error(e, exc_info=True)
             failed_pages.append(page)
 
-    logging.info("Downloaded pages with {} failures".format(len(failed_pages)))
+    logging.info(f"Downloaded pages with {len(failed_pages)} failures")
 
     with open("failures.csv", "w") as outfile:
         for failure in failed_pages:
-            outfile.write("{}\n".format(failure))
-    logging.info("Written failed pages to failures.csv")
+            outfile.write(f"{failure}\n")
+
+    logging.info("Written failed pages to `failures.csv`")
+    logging.info("Rerun the script will attempt to download pages in `failures.csv` again")
 
 
 if __name__ == "__main__":
-    download_zinc15()
+    parser = argparse.ArgumentParser(description="ZINC15 SDF Downloader")
+    parser.add_argument(
+        "--output",
+        type=str,
+        dest="sdf_path",
+        default=_sdf_path,
+        help="Path where downloaded `sdf` should be written written to",
+    )
+
+    args = parser.parse_args()
+
+    download_zinc15(sdf_path=args.sdf_path)
