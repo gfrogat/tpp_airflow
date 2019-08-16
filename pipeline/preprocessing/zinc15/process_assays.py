00da333c1c65df94daab48a914d90b873b92bf2d
Peter Ruch <gfrogat@gmail.com>
2019-08-16 19:10:46 +0200

    Cleanup ZINC15 preprocessing

--- pipeline/preprocessing/zinc15/process_assays.py
+++ pipeline/preprocessing/zinc15/process_assays.py
@@ -4,11 +4,12 @@ from pyspark.sql import functions as F
 
 from pathlib import Path
 
-spark = (
-    SparkSession.builder.appName("Process ZINC15 Assays")
-        .config("spark.sql.execution.arrow.enabled", "true")
-        .getOrCreate()
-)
+spark = SparkSession \
+    .builder \
+    .appName("Process ZINC15 Assays") \
+    .config("spark.sql.execution.arrow.enabled", "true") \
+    .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer") \
+    .getOrCreate()
 
 _data_root = Path("/local00/bioinf/tpp")
 
@@ -28,8 +29,11 @@ df = df.withColumn(
 ).filter(F.col("rank") == 1)
 """
 
-triples = df.groupby(["mol_id", "gene_name"]).agg(
-    F.mean("affinity").alias("avg_affinity"), F.collect_set("mol_file").alias("mol_file"))
+triples = df \
+    .groupby(["mol_id", "gene_name"]) \
+    .agg(F.mean("affinity").alias("avg_affinity"),
+         F.collect_set("mol_file").alias("mol_file"),
+         F.collect_set("inchikey").alias("inchikey"))
 
 target_size = df.select("gene_name").distinct().count()
 compound_size = df.select("mol_id").distinct().count()
@@ -38,28 +42,14 @@ compound_size = df.select("mol_id").distinct().count()
 assert df.select("gene_name").distinct().intersect(triples.select("gene_name").distinct()).count() == target_size
 assert df.select("mol_id").distinct().intersect(triples.select("mol_id").distinct()).count() == compound_size
 
-triples = triples.withColumn(
-    "activity", F.when(F.col("avg_affinity") > 8, 1).otherwise(-1)
-)
+triples = triples \
+    .withColumn("activity", F.when(F.col("avg_affinity") > 8, 1).otherwise(-1))
 
 w = Window.partitionBy("gene_name")
-
-triples = (
-    triples.withColumn("actives", F.count(F.when(F.col("activity") == 1, 0)).over(w))
-        .withColumn("inactives", F.count(F.when(F.col("activity") == -1, 0)).over(w))
-        .filter(
-        (F.col("actives") > 10)
-        & (F.col("inactives") > 10)
-        & (F.col("actives") + F.col("inactives") > 25)
-    )
-        .select(["mol_id", "mol_file", "gene_name", "activity"])
-)
-
-"""
-triples = triples.withColumn("mol_file_size", F.size("mol_file")).filter(F.col("mol_file_size") == 1).select("mol_id",
-                                                                                                       "mol_file",
-                                                                                                       "gene_name",
-                                                                                                       "activity")
-"""
+triples = triples \
+    .withColumn("actives", F.count(F.when(F.col("activity") == 1, 0)).over(w)) \
+    .withColumn("inactives", F.count(F.when(F.col("activity") == -1, 0)).over(w)) \
+    .filter((F.col("actives") > 10) & (F.col("inactives") > 10) & (F.col("actives") + F.col("inactives") > 25) \
+            .select(["mol_id", "inchikey", "mol_file", "gene_name", "activity"])
 
 triples.write.parquet((_data_root / "ZINC15/zinc15_data_cleaned.parquet").as_posix())
