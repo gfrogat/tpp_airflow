a1d49c2f0e591aa5dbcf0c53862f544edf9acfd7
Peter Ruch <gfrogat@gmail.com>
2019-08-16 19:11:28 +0200

    Cleanup dataset merging

--- pipeline/preprocessing/merge_assays.py
+++ pipeline/preprocessing/merge_assays.py
@@ -1,115 +1,195 @@
-from pyspark.sql import SparkSession
-from pyspark.sql import functions as F
-from pyspark.sql import types as T
-from pyspark.sql import Window
-
 from pathlib import Path
 
-spark = (
-    SparkSession.builder.appName("Process ChEMBL25 Assays")
-        .config("spark.sql.execution.arrow.enabled", "true")
-        .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
-        .getOrCreate()
-)
+from pyspark.sql import SparkSession
+from pyspark.sql import Window
+from pyspark.sql import functions as F
+from pyspark.sql import types as T
 
 _data_root = Path("/local00/bioinf/tpp")
 
-# PubChem - load assays
-pubchem_assays = spark.read.parquet(
-    (_data_root / "pubchem_20190717/pubchem_processed.parquet").as_posix()
-)
-
-# PubChem - load compounds
-pubchem_compounds = spark.read.parquet(
-    (_data_root / "pubchem_20190717/pubchem_compounds.parquet").as_posix()
-)
-
-# ChEMBL - load assays
-chembl_assays = spark.read.parquet(
-    (_data_root / "chembl_25/chembl_25_assays_cleaned.parquet").as_posix()
-)
-
-# Reformat activity column
-chembl_assays = (
-    chembl_assays
-        .filter(F.col("activity").isin([1, 3]))
-        .withColumn("activity", F.col("activity") - 2)
-)
-
-# Only use assays that with more than 100 measurements
-w = Window.partitionBy("assay_id")
-chembl_assays = (
-    chembl_assays.select("assay_id", "mol_id", "activity")
-        .withColumn("count", F.count("*").over(w))
-        .filter(F.col("count") > 100)
-        .drop("count")
-)
-
-# ChEMBL - load compounds
-chembl_compounds = spark.read.parquet(
-    (_data_root / "chembl_25/chembl_25_compounds.parquet").as_posix()
-)
-
-# Assay IDs
-pubchem_ids = (
-    pubchem_assays.select("aid")
-        .distinct()
-        .sort(F.asc("aid"))
-        .withColumn("assay_id", F.col("aid").cast(T.StringType()))
-        .drop("aid")
-)
-
-chembl_ids = chembl_assays.select("assay_id").distinct().sort(F.asc("assay_id"))
-
-assay_id_schema = T.StructType(
+merged_assay_ids_schema = T.StructType(
     [
         T.StructField("assay_id", T.StringType(), False),
+        T.StructField("dataset", T.StringType(), False),
         T.StructField("global_id", T.IntegerType(), False),
     ]
 )
 
-assay_ids = pubchem_ids.union(chembl_ids)
-assay_ids = assay_ids.rdd.map(lambda x: x[0]).zipWithIndex().toDF(assay_id_schema)
-assay_ids.write.parquet((_data_root / "merged_assay_ids.parquet").as_posix())
 
-# Compound IDs
-pubchem_compound_ids = pubchem_compounds.select("inchikey", "mol_id").sort(F.asc("mol_id"))
+# TODO verify with preprocessed TOX data
+def get_zinc15(zinc15_data_path, data_root=_data_root):
+    # ZINC15 - load data
+    data = spark.read.parquet(
+        (data_root / zinc15_data_path).as_posix()
+    )
 
-chembl_compound_ids = chembl_compounds.select("inchikey", "mol_id").sort(
-    F.asc("mol_id")
-)
+    # Compound IDs
+    compound_ids = data \
+        .select("inchikey", "mol_id") \
+        .dropDuplicates() \
+        .sort(F.asc("mol_id"))
 
-compound_ids = pubchem_compound_ids.union(chembl_compound_ids)
-compound_ids.write.parquet((_data_root / "merged_compound_ids.parquet").as_posix())
+    # Assay IDs
+    assay_ids = data \
+        .select("gene_name") \
+        .distinct() \
+        .sort(F.asc("gene_name")) \
+        .withColumn("assay_id", F.col("gene_name")) \
+        .drop("gene_name")
 
-# Merge PubChem
-pubchem_assays = pubchem_assays.alias("pa").join(
-    pubchem_compounds.alias("pc"), F.col("pa.cid") == F.col("pc.mol_id")
-)
-pubchem_assays = pubchem_assays.withColumn("dataset", F.lit("PubChem"))
-pubchem_assays = pubchem_assays.withColumn(
-    "assay_id", F.col("aid").cast(T.StringType())
-)
+    # Data
+    data = data \
+        .withColumn("dataset", F.lit("ZINC15")) \
+        .withColumn("assay_id", F.col("gene_name")) \
+        .select("inchikey", "assay_id", "mol_file", "activity", "dataset")
 
-pubchem_assays = pubchem_assays.select(
-    "inchikey", "assay_id", "mol_file", "activity", "dataset"
-)
+    return data, compound_ids, assay_ids
 
-# Merge ChEMBL
-chembl_assays = chembl_assays.alias("ca").join(
-    chembl_compounds.alias("cc"), F.col("ca.mol_id") == F.col("cc.mol_id")
-)
-chembl_assays = chembl_assays.withColumn("dataset", F.lit("ChEMBL"))
 
-chembl_assays = chembl_assays.select(
-    "inchikey", "assay_id", "mol_file", "activity", "dataset"
-)
+def get_pubchem(pubchem_compounds_path, pubchem_assays_path, data_root=_data_root):
+    # PubChem - load compounds
+    compounds = spark.read.parquet(
+        (data_root / pubchem_compounds_path).as_posix()
+    )
 
-# Merge Assays
-assays = pubchem_assays.union(chembl_assays)
-merged_assays = (
-    assays.alias("a")
-        .join(assay_ids.alias("ai"), F.col("a.assay_id") == F.col("ai.assay_id"))
-        .select("inchikey", "mol_file", "global_id", "activity", "dataset")
-)
-merged_assays.write.parquet((_data_root / "merged_assays.parquet").as_posix())
+    # PubChem - load assays
+    assays = spark.read.parquet(
+        (data_root / pubchem_assays_path).as_posix()
+    )
+
+    # Compound IDs
+    compound_ids = compounds \
+        .select("inchikey", "mol_id") \
+        .sort(F.asc("mol_id")) \
+        .withColumn("dataset", F.lit("PubChem"))
+
+    # Assay IDs
+    assay_ids = assays \
+        .select("aid") \
+        .distinct() \
+        .sort(F.asc("aid")) \
+        .withColumn("assay_id", F.col("aid").cast(T.StringType())) \
+        .drop("aid") \
+        .withColumn("dataset", F.lit("PubChem"))
+
+    # PubChem - Merge Compounds and Assays
+    data = assays.alias("a") \
+        .join(compounds.alias("c"), F.col("a.cid") == F.col("c.mol_id"))
+
+    data = data \
+        .withColumn("dataset", F.lit("PubChem")) \
+        .withColumn("assay_id", F.col("aid").cast(T.StringType())) \
+        .select("inchikey", "assay_id", "mol_file", "activity", "dataset")
+
+    return data, compound_ids, assay_ids
+
+
+def get_chembl(chembl_compounds_path, chembl_assays_path, data_root=_data_root):
+    # ChEMBL - load compounds
+    compounds = spark.read.parquet(
+        (data_root / chembl_compounds_path).as_posix()
+    )
+
+    # ChEMBL - load assays
+    assays = spark.read.parquet(
+        (data_root / chembl_assays_path).as_posix()
+    )
+
+    # Reformat activity column
+    assays = assays \
+        .filter(F.col("activity").isin([1, 3])) \
+        .withColumn("activity", F.col("activity") - 2)
+
+    # Only use assays with more than 100 measurements
+    w = Window.partitionBy("assay_id")
+    assays = assays \
+        .select("assay_id", "mol_id", "activity") \
+        .withColumn("count", F.count("*").over(w)) \
+        .filter(F.col("count") > 100) \
+        .drop("count") \
+        .withColumn("dataset", F.lit("ChEMBL"))
+
+    # Compound IDs
+    compound_ids = compounds \
+        .select("inchikey", "mol_id") \
+        .sort(F.asc("mol_id")) \
+        .withColumn("dataset", F.lit("ChEMBL"))
+
+    # Assay IDs
+    assay_ids = assays \
+        .select("assay_id") \
+        .distinct() \
+        .sort(F.asc("assay_id")) \
+        .withColumn("dataset", F.lit("ChEMBL"))
+
+    # ChEMBL - Merge Compounds and Assays
+    data = assays.alias("a") \
+        .join(compounds.alias("c"), F.col("a.mol_id") == F.col("c.mol_id"))
+
+    data = data \
+        .withColumn("dataset", F.lit("ChEMBL")) \
+        .select("inchikey", "assay_id", "mol_file", "activity", "dataset")
+
+    return data, compound_ids, assay_ids
+
+
+if __name__ == '__main__':
+    pubchem_compounds_path = "pubchem_20190717/pubchem_compounds.parquet"
+    pubchem_assays_path = "pubchem_20190717/pubchem_processed.parquet"
+
+    chembl_compounds_path = "chembl_25/chembl_25_compounds.parquet"
+    chembl_assays_path = "chembl_25/chembl_25_assays_cleaned.parquet"
+
+    zinc15_data_path = "ZINC15/zinc15_data_full.parquet"
+
+    try:
+        spark = SparkSession \
+            .builder \
+            .appName("Merge ChEMBL + PubChem") \
+            .config("spark.sql.execution.arrow.enabled", "true") \
+            .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer") \
+            .getOrCreate()
+
+        pubchem_data, pubchem_compound_ids, pubchem_assay_ids = get_pubchem(pubchem_compounds_path, pubchem_assays_path)
+        chembl_data, chembl_compound_ids, chembl_assay_ids = get_chembl(chembl_compounds_path, chembl_assays_path)
+
+        # Export assay_id, global_id mapping
+        merged_assay_ids = pubchem_assay_ids \
+            .union(chembl_assay_ids)
+
+        merged_assay_ids = merged_assay_ids \
+            .rdd \
+            .zipWithIndex() \
+            .map(lambda x: (*x[0], x[1])) \
+            .toDF(merged_assay_ids_schema)
+
+        merged_assay_ids \
+            .write \
+            .parquet((_data_root / "merged_assay_ids.parquet").as_posix())
+
+        # Export compound_id, inchikey mapping
+        merged_compound_ids = pubchem_compound_ids.union(chembl_compound_ids)
+        merged_compound_ids \
+            .write \
+            .parquet((_data_root / "merged_compound_ids.parquet").as_posix())
+
+        # Merge Assays
+        merged_data = pubchem_data.union(chembl_data)
+        merged_data = merged_data.alias("md") \
+            .join(merged_assay_ids.select("assay_id", "global_id").alias("ma"),
+                  F.col("md.assay_id") == F.col("ma.assay_id")) \
+            .select("inchikey", "mol_file", "global_id", "activity", "dataset")
+
+        flattened_data = merged_data \
+            .groupby("inchikey") \
+            .agg(
+            F.collect_set("mol_file").alias("mol_file"),
+            F.map_from_entries(
+                F.sort_array(F.collect_list(
+                    F.struct(F.col("global_id"), F.col("activity")))).alias("labels")))
+
+        flattened_data.write.parquet((_data_root / "merged_data.parquet").as_posix())
+    except Exception:
+    # handle Exception
+    finally:
+        spark.stop()
