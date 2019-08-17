0ff4afba12de9b10e76b71ee8040b84989bb1935
Peter Ruch <gfrogat@gmail.com>
2019-08-17 11:09:23 +0200

    Create new folder scripts

--- scripts/download_pubchem.sh
+++ scripts/download_pubchem.sh
@@ -4,7 +4,7 @@ PUBCHEM_DIR="/publicdata/tpp/datasets/PubChem"
 PUBCHEM_BIOASSAY_URL="ftp://ftp.ncbi.nlm.nih.gov/pubchem/Bioassay/CSV/Data"
 PUBCHEM_COMPOUND_URL="ftp://ftp.ncbi.nlm.nih.gov/pubchem/Compound/CURRENT-Full/SDF"
 
-PUBCHEM_VERSION="pubchem_"`date +"%Y%m%d"`
+PUBCHEM_VERSION="pubchem_"$(date +"%Y%m%d")
 
-wget -mP ${PUBCHEM_DIR}/${PUBCHEM_VERSION} ${PUBCHEM_BIOASSAY_URL}
-wget -mP ${PUBCHEM_DIR}/${PUBCHEM_VERSION} ${PUBCHEM_COMPOUND_URL}
+wget -mP ${PUBCHEM_DIR}/"${PUBCHEM_VERSION}" ${PUBCHEM_BIOASSAY_URL}
+wget -mP ${PUBCHEM_DIR}/"${PUBCHEM_VERSION}" ${PUBCHEM_COMPOUND_URL}
--- scripts/download_tox21.sh
+++ scripts/download_tox21.sh
@@ -5,6 +5,6 @@ TOX21_ALL_URL="https://tripod.nih.gov/tox21/challenge/download?id=tox21_10k_data
 TOX21_TEST_URL="https://tripod.nih.gov/tox21/challenge/download?id=tox21_10k_challenge_testsdf"
 TOX21_SCORE_URL="https://tripod.nih.gov/tox21/challenge/download?id=tox21_10k_challenge_scoresdf"
 
-wget -P ${TOX21_DIR} -O tox21_10k_data_all.sdf.zip ${TOX21_ALL_URL}
-wget -P ${TOX21_DIR} -O tox21_10k_data_test.sdf.zip ${TOX21_TEST_URL}
-wget -P ${TOX21_DIR} -O tox21_10k_data_score.sdf.zip ${TOX21_SCORE_URL}
+wget -P ${TOX21_DIR} -O tox21_10k_data_all.sdf.zip "${TOX21_ALL_URL}"
+wget -P ${TOX21_DIR} -O tox21_10k_data_test.sdf.zip "${TOX21_TEST_URL}"
+wget -P ${TOX21_DIR} -O tox21_10k_data_score.sdf.zip "${TOX21_SCORE_URL}"
