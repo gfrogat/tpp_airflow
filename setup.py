19b5736bb6c7ac1f27332bc8dfd491ad6a14fa18
Peter Ruch <gfrogat@gmail.com>
2019-08-18 12:36:20 +0200

    Enable pip install

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    # Metadata
+    name="tpp",
+    author="Peter Ruch",
+    author_email="gfrogat@gmail.com",
+    license="MIT",
+    description="Target Prediction Pipeline",
+    # Package info
+    packages=find_packages(),
+    zip_safe=False,
+)
