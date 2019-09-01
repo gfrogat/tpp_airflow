ca68d1d65f474a63be2e0a285ad8c28c5b14476d
Peter Ruch <gfrogat@gmail.com>
2019-09-01 15:34:06 +0200

    Update download_pubchem script

--- scripts/download_pubchem.sh
+++ scripts/download_pubchem.sh
@@ -4,14 +4,18 @@ PUBCHEM_DIR="/publicdata/tpp/datasets/PubChem"
 PUBCHEM_BIOASSAY_URL="ftp://ftp.ncbi.nlm.nih.gov/pubchem/Bioassay/CSV/Data"
 PUBCHEM_COMPOUND_URL="ftp://ftp.ncbi.nlm.nih.gov/pubchem/Compound/CURRENT-Full/SDF"
 
-PUBCHEM_VERSION="pubchem_"$(date +"%Y%m%d")
-
-
 if [[ -z ${PUBCHEM_DIR} ]]; then
     echo "PubChem output dir is not set";
     echo "Set PUBCHEM_DIR evironment variable and rerun script";
     echo "Example: PUBCHEM_DIR=/data bash download_pubchem.sh"
     exit
+fi
+
+if [[ -z ${PUBCHEM_VERSION} ]]; then
+    echo "PubChem version is not set";
+    PUBCHEM_VERSION="pubchem_"$(date +"%Y%m%d")
+    echo "Using date as for versioning: ${PUBCHEM_VERSION}"
+    printf "\n#############################################################\n"
 fi
 
 wget -mP "${PUBCHEM_DIR}/${PUBCHEM_VERSION}" "${PUBCHEM_BIOASSAY_URL}"
