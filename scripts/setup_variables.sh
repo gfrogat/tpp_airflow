a10edf7a2dd3fbbed5ab402779110f0f86317c22
Peter Ruch <gfrogat@gmail.com>
2019-11-21 20:12:45 +0100

    Add setup_variables.sh

---
+++ scripts/setup_variables.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+DB_HOST="demosite.ml.jku.at"
+DB_HOST_DOCKER_IP="172.18.0.1"
+AIRFLOW_ENV=".docker.env"
+AIRFLOW_NETWORK="tpp"
+SECRETS_DIR="/run/secrets"
+
+docker run --rm \
+    --env-file "${AIRFLOW_ENV}" \
+    --network "${AIRFLOW_NETWORK}" \
+    --add-host "${DB_HOST}:${DB_HOST_DOCKER_IP}" \
+    -v "$(pwd)"/secrets/ca-cert.pem:${SECRETS_DIR}/ca-cert \
+    -v "$(pwd)"/secrets/client-key.pem:"${SECRETS_DIR}"/postgres-client-key \
+    -v "$(pwd)"/secrets/client-cert.pem:"${SECRETS_DIR}"/postgres-client-cert \
+    -v "$(pwd)"/secrets/client-key.pem:"${SECRETS_DIR}"/rabbitmq-client-key \
+    -v "$(pwd)"/secrets/client-cert.pem:"${SECRETS_DIR}"/rabbitmq-client-cert \
+    -ti ml-jku/airflow \
+    airflow connections --add --conn_id spark_raptor --conn_type spark --conn_host spark://raptor:7077 --conn_extra '{"queue": "root.default"}'
+
+docker run --rm \
+    --env-file "${AIRFLOW_ENV}" \
+    --network "${AIRFLOW_NETWORK}" \
+    --add-host "${DB_HOST}:${DB_HOST_DOCKER_IP}" \
+    -v "$(pwd)"/secrets/ca-cert.pem:${SECRETS_DIR}/ca-cert \
+    -v "$(pwd)"/secrets/client-key.pem:"${SECRETS_DIR}"/postgres-client-key \
+    -v "$(pwd)"/secrets/client-cert.pem:"${SECRETS_DIR}"/postgres-client-cert \
+    -v "$(pwd)"/secrets/client-key.pem:"${SECRETS_DIR}"/rabbitmq-client-key \
+    -v "$(pwd)"/secrets/client-cert.pem:"${SECRETS_DIR}"/rabbitmq-client-cert \
+    -v "$(pwd)"/configs:/configs \
+    -ti ml-jku/airflow \
+    airflow variables --import /configs/dag_variables.json
